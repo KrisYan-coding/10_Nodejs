cb1cd3e673e71c9156fd73ad0ad97ed37a947536
KrisYan <krisyan3318@gmail.com>
2022-12-27 15:24:48 +0800

    express static

--- class/index.js
+++ class/index.js
@@ -7,7 +7,15 @@ const app = express();
 
 // 路由設定, routes--
 app.get('/', (req, res) => {
-    res.send(`<h1>你好</h1>`)
+    res.send(`<h1>你好yoyo</h1>`)
+});
+
+app.use(express.static('public'));
+
+// ***** 所有的路由要放在這行之前 ***
+app.use('/', (req, res) => {
+    res.type('text/plain');
+    res.status(404).send(`找不到你要的頁面yo`);
 });
 
 const port = process.env.PORT || 3001;
