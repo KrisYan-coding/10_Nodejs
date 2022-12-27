507aecf340993b7d4412e8789a9796d52e854a57
KrisYan <krisyan3318@gmail.com>
2022-12-27 15:08:29 +0800

    express

---
+++ class/index.js
@@ -0,0 +1,17 @@
+
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+// 路由設定, routes--
+app.get('/', (req, res) => {
+    res.send(`<h1>你好</h1>`)
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`server start: ${port}`);
+});
+
---
+++ class/src/15_1227_list-my-env.js
@@ -0,0 +1,3 @@
+
+console.log(process.env.MY_VAR);
+// SET MY_VAR = 您好 && node src\15_1227_list-my-env.js
