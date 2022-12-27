d899ee06f312086e833a6192016bd10a2b93247b
KrisYan <krisyan3318@gmail.com>
2022-12-27 16:41:51 +0800

    last

--- class/index.js
+++ class/index.js
@@ -5,10 +5,22 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 // 路由設定, routes--
 app.get('/', (req, res) => {
-    res.send(`<h1>你好yoyo</h1>`)
+    res.render('main', { name: '陳小名' });
 });
+
+app.get('/json-sales', (req, res) => {
+    const data = require(__dirname + '/data/sales.json');
+    res.json(data);
+    // res.render('json-sales');
+});
+
+// app.get('/a.html', (req, res) => {
+//     res.send(`<h1>假的 a.html</h1>`)
+// });
 
 app.use(express.static('public'));
 
